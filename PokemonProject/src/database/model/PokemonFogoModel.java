8da70fa6cc70dd1da7c99974520220e1fa41199f
Karen Bialescki Stackoski <stackoskikaren@gmail.com>
2024-06-11 20:56:38 -0300

    models finais

--- PokemonProject/src/database/model/PokemonFogoModel.java
+++ PokemonProject/src/database/model/PokemonFogoModel.java
@@ -1,5 +1,26 @@
 package database.model;
 
 public class PokemonFogoModel {
+	private int id;
+	private String pokemonFogo;
+	
+	public PokemonFogoModel() {
+		
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPokemonFogo() {
+		return pokemonFogo;
+	}
+
+	public void setPokemonFogo(String pokemonFogo) {
+		this.pokemonFogo = pokemonFogo;
+	}
 }
--- PokemonProject/src/database/model/PokemonVoadorModel.java
+++ PokemonProject/src/database/model/PokemonVoadorModel.java
@@ -1,5 +1,26 @@
 package database.model;
 
 public class PokemonVoadorModel {
+	private int id;
+	private String pokemonVoador;
+	
+	public PokemonVoadorModel() {
+		
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPokemonVoador() {
+		return pokemonVoador;
+	}
+
+	public void setPokemonVoador(String pokemonVoador) {
+		this.pokemonVoador = pokemonVoador;
+	}
 }
