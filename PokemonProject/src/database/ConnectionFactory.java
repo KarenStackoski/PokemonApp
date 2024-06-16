fb768d5ce85ba009af04d1037c61371db3e253bf
luisgabrielMR <luisgabrielmr2019@gmail.com>
2024-06-16 18:57:28 -0300

    Inicio da criação da conexão para com o Banco
    Ainda criando a conexão pode ainda não estar funcionando.

--- PokemonProject/src/database/ConnectionFactory.java
+++ PokemonProject/src/database/ConnectionFactory.java
@@ -1,5 +1,30 @@
 package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionFactory {
 
+    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String USER = "root";
+    private static final String PASSWORD = "unesc";
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Falha ao carregar o driver JDBC", e);
+        }
+    }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
