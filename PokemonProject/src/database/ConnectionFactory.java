2e3be5e8e0c429099123f69199034624c5ebe705
luisgabrielMR <luisgabrielmr2019@gmail.com>
2024-06-18 19:44:37 -0300

    Update de correção
    correção simples na conexão

--- PokemonProject/src/database/ConnectionFactory.java
+++ PokemonProject/src/database/ConnectionFactory.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String URL = "jdbc:mysql://localhost:3306/Pokemon";
     private static final String USER = "root";
     private static final String PASSWORD = "unesc";
     static {
